b610abb5e8366fdc75d9c512aa554b05c781a0de
Aristi <aristizabal.ocampo@gmail.com>
2023-03-08 21:36:33 -0500

    cargando archivo config.json

--- nukeldr.js
+++ nukeldr.js
@@ -1,10 +1,11 @@
 const axios = require('axios');
+const {token} =require('./config.json');
 
 solicitud()
 
 async function solicitud(){
 
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVkQXQiOjE2NzgzMjc0NTA2NzAsImVtYWlsIjoiYXJpc3RpemFiYWwub2NhbXBvQGdtYWlsLmNvbSIsImFjdGlvbiI6InRva2VuLWFwaSIsImlhdCI6MTY3ODMyNzQ1MH0.lS4bSC2mV8RombbWc_tFu8mAhbsCW-LpqEIPn1snspM';
+    
     const response = await axios.get('https://public-api.solscan.io/account/tokens?account=4KjxgEds5XEG1wuLZQYGqY87w9KuxpRjkBF65xn8rHQt',{
         headers: {
             'accept': 'application/json',
